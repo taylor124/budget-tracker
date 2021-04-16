bfadf9e27b6b4cd66b4f122a88163d6517ff8cab
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-04-16 11:35:14 -0400

    Bug fixes

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -6,7 +6,8 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/css/styles.css",
     "/js/index.js",
-    "/js/idb.js"
+    "/js/idb.js",
+    "/models/transaction.js"
 ];
 
 self.addEventListener('install', function (e) {
